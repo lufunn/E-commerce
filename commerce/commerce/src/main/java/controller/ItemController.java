cf16460796c2bc97acf03df7cba796d632dfb5bd
lufunn <lufuno.lucky.mbedzi@gmail.com>
2024-08-02 15:59:08 +0200

    java assignment

---
+++ commerce/commerce/src/main/java/controller/ItemController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import entity.Item;
+import service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping
+    public List<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+        return itemService.getItemById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Item addItem(@RequestBody Item item) {
+        return itemService.addItem(item);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> updateItem(@PathVariable Long id, @RequestBody Item itemDetails) {
+        return ResponseEntity.ok(itemService.updateItem(id, itemDetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteItem(@PathVariable Long id) {
+        itemService.deleteItem(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ commerce/commerce/src/main/java/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+
+import entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long>  {
+}
